ce0a578be9dc4bb639dd0bb43088245d33fdf491
Andrew Neish <amneish@stanford.edu>
2019-08-12 19:52:54 -0700

    Added .gitignore and added testing directory

---
+++ Debug Files/testAll.m
@@ -0,0 +1,29 @@
+% Test all
+clear; close all; clc;
+
+%% Get test file names to be run
+
+testDir = [pwd, '\debugScripts'];
+testList = dir(fullfile(testDir, '*.m'));
+
+% Record command prompt
+if (exist('testResults.test', 'file') == 2)
+    delete testResults.test
+end
+
+diary testResults.test
+
+%% Test sgt
+for i = 1:length(testList)
+    run(fullfile(testDir, testList(i).name));
+end
+
+license('inuse') %  This helps in development to ensure that on MATLAB is being used.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%% Check to see that only the 'matlab' license is used %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+diary off
+
+
