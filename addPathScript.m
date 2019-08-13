ba0eca9b6ef447ca4f64ec8f7eed0bf5b9a066d8
Andrew Neish <amneish@stanford.edu>
2019-08-12 20:00:50 -0700

    Added addPathScript.m to the git root directory

---
+++ addPathScript.m
@@ -0,0 +1,13 @@
+% Add relevent paths to run maast tool
+% Keep this file in the git root directory
+%
+% Note: This file only adds the maast tools to the path. Most maast
+% functions require the stanford-gnss-tool which can be found at:
+% https://github.com/stanford-gps-lab/stanford-gnss-tools
+% 
+% Once you have downloaded this set of code to a separate directory, use
+% the addPathScript associated with that code base to add those tools to
+% the path as well.
+
+addpath('maast')
+addpath('stanfordgnsstools\PolygonFiles')
