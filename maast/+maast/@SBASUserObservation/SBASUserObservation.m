2d830e0c712b461c197272b4aeb5edf9fbbd1625
Andrew Neish <amneish@stanford.edu>
2019-08-16 16:58:40 -0700

    Began to write maast.SBASUserObservation

---
+++ maast/+maast/@SBASUserObservation/SBASUserObservation.m
@@ -0,0 +1,13 @@
+classdef SBASUserObservation < sgt.UserObservation
+    % SBASUserObservation   a container for an observation from an SBAS
+    % user.
+    %
+    %   maast.SBASUserObservation(sbasUser, satellitePosition) creates 
+    
+    
+    
+    
+    
+    
+    
+end
