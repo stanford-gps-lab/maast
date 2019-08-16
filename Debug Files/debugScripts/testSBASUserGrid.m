a15abb237f386d2aaaab5b0201431e6ea5f5c9f8
Andrew Neish <amneish@stanford.edu>
2019-08-16 13:30:05 -0700

    Added testSBASUserGrid

---
+++ Debug Files/debugScripts/testSBASUserGrid.m
@@ -0,0 +1,63 @@
+function testSBASUserGrid()
+fprintf('Testing maast.SBASUserGrid: ')
+
+testResults = [];
+%% Define Test Parameters
+posLLH = [1 2 3; 4 5 6];
+polygonFile = 'usrconus.dat';
+gridName = 'MyGrid';
+
+sgtUserGrid = sgt.UserGrid.createUserGrid('NumUsers', 100);
+
+%% Test 1 - Constructor - single SBASUserGrid
+try
+    test1 = maast.SBASUserGrid(posLLH);
+    
+    if (~isa(test1, 'maast.SBASUserGrid')) && (~isa(test1.Users, 'maast.SBASUser'))
+        testResults(1) = 1;
+    end
+catch
+    testResults(1) = 1;
+end
+
+%% Test 2 - Constructor - varargin: GridName
+try
+    test2 = maast.SBASUserGrid(posLLH, 'GridName', gridName);
+    
+    if (~strcmp(test2.GridName, gridName))
+        testResults(2) = 1;
+    end
+catch
+    testResults(2) = 1;
+end
+
+%% Test 3 - Constructor - varargin: PolygonFile
+try
+    test3 = maast.SBASUserGrid(posLLH, 'PolygonFile', polygonFile);
+catch
+    testResults(3) = 1;
+end
+
+%% Test 4 - SBASUserGrid.fromsgtUserGrid - single sgt.UserGrid
+try
+    test4 = maast.SBASUserGrid.fromsgtUserGrid(sgtUserGrid);
+    
+    if (~isa(test4, 'maast.SBASUserGrid')) && (~isa(test4.Users, 'maast.SBASUser'))
+        testResults(4) = 1;
+    end
+catch
+    testResults(4) = 1;
+end
+
+%% Display test results
+if any(testResults)
+    fprintf('---Failed---\n')
+    testResults = find(testResults);
+    for i = 1:length(testResults)
+        fprintf(['test', num2str(testResults(i)), ' failed\n'])
+    end
+else
+    fprintf('Passed\n')
+end
+
+end
