4c7f17672ed41f8a4b536d76438950b070f7a19c
Andrew Neish <amneish@stanford.edu>
2019-08-16 13:14:59 -0700

    Finished initial implementation of testSBASUser

--- Debug Files/debugScripts/testSBASUser.m
+++ Debug Files/debugScripts/testSBASUser.m
@@ -4,12 +4,85 @@ fprintf('Testing maast.SBASUser: ')
 testResults = [];
 %% Define Test Parameters
 posLLH = [1 2 3];
+posLLH2 = [posLLH; 4 5 6];
+[numUsers2, ~] = size(posLLH2);
+polygonFile = 'usrconus.dat';
+elevationMask = 10*pi/180;
+
+sgtUser = sgt.User(posLLH);
+sgtUser2 = sgt.User(posLLH2);
 
 %% Test 1 - Constructor - single SBAS User
 try
-   test1 = maast.SBASUser(posLLH); 
+    test1 = maast.SBASUser(posLLH);
+    
+    if (~isa(test1, 'maast.SBASUser'))
+        testResults(1) = 1;
+    end
 catch
     testResults(1) = 1;
+end
+
+%% Test 2 - Constructor - multiple SBAS Users
+try
+    test2 = maast.SBASUser(posLLH2);
+    
+    if (~isa(test2, 'maast.SBASUser')) && (length(test2) ~= numUsers2)
+        testResults(2) = 1;
+    end
+catch
+    testResults(2) = 1;
+end
+
+%% Test 3 - Constructor - varargin: ID
+try
+    test3 = maast.SBASUser(posLLH, 'ID', 3);
+    
+    if (test3.ID ~= 3)
+        testResults(3) = 1;
+    end
+catch
+    testResults(3) = 1;
+end
+
+%% Test 4 - Constructor - varargin: PolygonFile
+try
+    test4 = maast.SBASUser(posLLH, 'PolygonFile', polygonFile);
+catch
+    testResults(4) = 1;
+end
+
+%% Test 5 - Constructor - varargin: ElevationMask
+try
+    test5 = maast.SBASUser(posLLH, 'ElevationMask', elevationMask);
+    
+    if (test5.ElevationMask ~= elevationMask)
+        testResults(5) = 1;
+    end
+catch
+    testResults(5) = 1;
+end
+
+%% Test 6 - SBASUser.fromsgtUser - single user
+try
+    test6 = maast.SBASUser.fromsgtUser(sgtUser);
+    
+    if (~isa(test6, 'maast.SBASUser'))
+        testResults(6) = 1;
+    end
+catch
+    testResults(6) = 1;
+end
+
+%% Test 7 - SBASUser.fromsgtUser - multiple users
+try
+    test7 = maast.SBASUser.fromsgtUser(sgtUser2);
+    
+    if (~isa(test7, 'maast.SBASUser')) && (length(test7) ~= length(sgtUser2))
+        testResults(7) = 1;
+    end
+catch
+    testResults(7) = 1;
 end
 
 %% Display test results
@@ -22,9 +95,5 @@ if any(testResults)
 else
     fprintf('Passed\n')
 end
-
-
-
-
 
 end
--- maast/+maast/@SBASUser/SBASUser.m
+++ maast/+maast/@SBASUser/SBASUser.m
@@ -4,8 +4,17 @@ classdef SBASUser < sgt.User
     %   maast.SBASUser(posLLH, varargin) creates an SBAS user at position
     %   posLLH. If posLLH is an Nx3 matrix, then N SBASUser objects will be
     %   created where each column in posLLH represents [lat lon alt] in
-    %   [deg deg m]. maast.SBASUser is a subclass of sgt.User and so has 
+    %   [deg deg m]. maast.SBASUser is a subclass of sgt.User and so has
     %   access to all public and protected methods defined for sgt.User.
+    %
+    %   varargin:
+    %   -----
+    %   ID - the ID of the user
+    %   -----
+    %   PolygonFile - specifies the name of a polyfile that bounds a
+    %   geographic region. See sgt.tools.generatePolygon
+    %   -----
+    %   ElevationMask - Elevation mask of users [rad]. Default 5 degrees.
     %
     %   See Also: sgt.User, sgt.UserGrid, maast.SBASUserGrid
     
