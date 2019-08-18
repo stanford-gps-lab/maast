4bfe728834d15f365a4b545ff74242028cfc568c
Andrew Neish <amneish@stanford.edu>
2019-08-18 08:23:06 -0700

    Began SBASUserObservation

--- maast/+maast/@SBASUserObservation/SBASUserObservation.m
+++ maast/+maast/@SBASUserObservation/SBASUserObservation.m
@@ -2,7 +2,25 @@ classdef SBASUserObservation < sgt.UserObservation
     % SBASUserObservation   a container for an observation from an SBAS
     % user.
     %
-    %   maast.SBASUserObservation(sbasUser, satellitePosition) creates 
+    %   maast.SBASUserObservation(sbasUser, satellitePosition) creates an
+    %   SBAS User Observation. maast.SBASUserObservation is a subclass of
+    %   sgt.UserObservation and contains additional properties and methods
+    %   needed for maast calculations.
+    %
+    %   See Also: sgt.UserObservation,
+    %   maast.SBASUserObservation.createSBASUserObservation, maast.SBASUser
+    
+    % Copyright 2019 Stanford University GPS Laboratory
+    %   This file is part of MAAST which is released under the MIT License.
+    %   See `LICENSE.txt` for full license details. Questions and comments
+    %   should be directed to the project at:
+    %   https://github.com/stanford-gps-lab/maast
+    
+    methods
+        function obj = SBASUserObservation()
+            
+        end
+    end
     
     
     
