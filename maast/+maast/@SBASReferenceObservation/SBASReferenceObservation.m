d047ba235e7dbdbf4ef04e877d67278073366d68
Andrew Neish <amneish@stanford.edu>
2019-08-19 19:23:51 -0700

    Added framework for SBASReferenceObservation

---
+++ maast/+maast/@SBASReferenceObservation/SBASReferenceObservation.m
@@ -0,0 +1,39 @@
+classdef SBASReferenceObservation < maast.SBASUserObservation
+    % SBASReferenceObservation  a container for an SBAS reference station
+    % observation.
+    %
+    %   maast.SBASReferenceObservation(sbasReferenceStation,
+    %   satellitePosition) creates an SBAS reference station observation
+    %   using a maast.SBASReferenceStation object and sgt.SatellitePosition
+    %   object.
+    %
+    %   See Also: sgt.UserObservation, maast.SBASUserObservation
+    
+    % Constructor
+    methods
+        function obj = SBASReferenceObservation(sbasReferenceStation, satellitePosition)
+            % Handle different number of arguments
+            args = {};
+            if (nargin == 1)
+                args = {sbasReferenceStation};
+            elseif (nargin > 1)
+                args = {sbasReferenceStation, satellitePosition};
+            end
+            
+            % Use superclass constructor
+            obj = obj@maast.SBASUserObservation(args{:});
+            
+        end
+    end
+    
+    % Public Methods
+    methods
+        sig2Tropo = tropoVariance(satellitePosition);
+    end
+    
+    % Static Methods
+    methods
+       obj = fromSBASUserObservation(sbasUserObservation); 
+    end
+    
+end
