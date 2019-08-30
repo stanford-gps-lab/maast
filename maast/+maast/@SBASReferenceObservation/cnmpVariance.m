96ef84163e2ee3795536af6244cb8607c9344dcb
Andrew Neish <amneish@stanford.edu>
2019-08-30 13:23:41 -0700

    SBASReferenceObservation.cnmpVariance now allows for multiple observations as an input

--- maast/+maast/@SBASReferenceObservation/cnmpVariance.m
+++ maast/+maast/@SBASReferenceObservation/cnmpVariance.m
@@ -9,7 +9,7 @@ function [] = cnmpVariance(obj)
 %   https://github.com/stanford-gps-lab/maast
 
 % Set Constants
-maastConstants = sgt.maast.constants.MAASTConstants;
+maastConstants = maast.constants.MAASTConstants;
 f1 = maastConstants.L1Frequency;
 f2 = maastConstants.L2Frequency;
 
@@ -18,12 +18,15 @@ cnmpCoef = [-3.9586 11.5991 -10.5790 0.7142];    % aggressive cnmp
 a1 = f1^2/(f1^2-f2^2);
 a2 = f2^2/(f1^2-f2^2);
 
-elevationAngles = obj.ElevationAngles;
-svInView = obj.SatellitesInViewMask;
-
-% Preallocate
-obj.Sig2CNMP = NaN(length(elevationAngles), 1);
-
-% Calculate CNMP
-obj.Sig2CNMP(svInView) = (a1^2 + a2^2) * exp(polyval(cnmp_coef,elevationAngles(svInView))).^2;
+for i = 1:length(obj)
+    
+    elevationAngles = obj(i).ElevationAngles;
+    svInView = obj(i).SatellitesInViewMask;
+    
+    % Preallocate
+    obj(i).Sig2CNMP = NaN(length(elevationAngles), 1);
+    
+    % Calculate CNMP
+    obj(i).Sig2CNMP(svInView) = (a1^2 + a2^2) * exp(polyval(cnmpCoef,elevationAngles(svInView))).^2;
+end
 end
