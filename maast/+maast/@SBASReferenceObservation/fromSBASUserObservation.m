0fb6701a0bf22ba944663d232ef2effdd885645c
Andrew Neish <amneish@stanford.edu>
2019-08-30 13:21:43 -0700

    Added fromSBASUserObservation.m

---
+++ maast/+maast/@SBASReferenceObservation/fromSBASUserObservation.m
@@ -0,0 +1,25 @@
+function obj = fromSBASUserObservation(sgtUserObservation)
+% fromsgtUserObservation creates a maast.SBASUserObservation from an
+% sgt.UserObservation
+
+% Copyright 2019 Stanford University GPS Laboratory
+%   This file is part of MAAST which is released under the MIT License.
+%   See `LICENSE.txt` for full license details. Questions and comments
+%   should be directed to the project at:
+%   https://github.com/stanford-gps-lab/maast
+
+% Preallocate
+obj(length(sgtUserObservation)) = maast.SBASReferenceObservation;
+
+% Allocate Properties
+[obj.UserID] = deal(sgtUserObservation.UserID);
+[obj.SatellitePRN] = deal(sgtUserObservation.SatellitePRN);
+[obj.t] = deal(sgtUserObservation.t);
+[obj.LOSecef] = deal(sgtUserObservation.LOSecef);
+[obj.LOSenu] = deal(sgtUserObservation.LOSenu);
+[obj.SatellitesInViewMask] = deal(sgtUserObservation.SatellitesInViewMask);
+[obj.ElevationAngles] = deal(sgtUserObservation.ElevationAngles);
+[obj.AzimuthAngles] = deal(sgtUserObservation.AzimuthAngles);
+[obj.NumSatellitesInView] = deal(sgtUserObservation.NumSatellitesInView);
+[obj.Range] = deal(sgtUserObservation.Range);
+end
