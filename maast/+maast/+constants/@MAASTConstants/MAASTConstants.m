c1ed8dc46e11cc064beefcfddf78db4807be9f29
Andrew Neish <amneish@stanford.edu>
2019-08-30 12:25:05 -0700

    Created class for constants specific to MAAST

---
+++ maast/+maast/+constants/@MAASTConstants/MAASTConstants.m
@@ -0,0 +1,16 @@
+classdef MAASTConstants < matlab.mixin.Copyable
+    % MAASTConstants     a container for constants specific to MAAST.
+    %    Grab a MAASTConstants object by using var =
+    %    maast.constants.MAASTConstants.
+    
+    properties
+        % L1Frequency - [Hz] L1 center frequency
+        L1Frequency = 1575.42e6;
+        
+        % L2Frequency - [Hz] L2 center frequency
+        L2Frequency = 1227.60e6;
+    end
+    
+        
+    
+end
