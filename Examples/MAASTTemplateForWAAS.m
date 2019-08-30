d432a5ebd0de0aef6e3647c26908536f30cfb399
Andrew Neish <amneish@stanford.edu>
2019-08-30 09:40:48 -0700

    Updated script

--- Examples/MAASTTemplateForWAAS.m
+++ Examples/MAASTTemplateForWAAS.m
@@ -9,6 +9,7 @@
 clear; close all; clc;
 
 %% Set Parameters
+fprintf('Setting up run...\n')
 posLLH = [37.427127, -122.173243, 17];  % [deg deg m] Stanford GPS Lab location
 waasReferenceStationPos = 'wrs_foc.dat';
 polyFile = 'usrconus.dat';
@@ -20,30 +21,36 @@ time = 0:300:86400;     % [s]
 timeLength = length(time);
 
 %% Build WAAS reference station Grid
-wrs = maast.SBASReferenceStationGrid.createReferenceStationGrid('LLHFile', waasReferenceStationPos);
+fprintf('Building WAAS reference station grid: ')
+wrsGrid = maast.SBASReferenceStationGrid.createReferenceStationGrid('LLHFile', waasReferenceStationPos);
+fprintf([num2str(length(wrsGrid.Users)), ' WAAS reference stations\n'])
 
 %% Build SBAS User Grid
+fprintf('Building WAAS user grid: ')
 sbasUserGrid = maast.SBASUserGrid.createUserGrid('PolygonFile', polyFile, 'GridStep', gridStep);
+fprintf([num2str(length(sbasUserGrid.Users)), ' WAAS users\n'])
 
 %% Build Satellite Constellation
+fprintf('Building satellite constellation: ')
 satellite = sgt.Satellite.fromYuma(almanac);
+fprintf([num2str(length(satellite)), ' satellites\n'])
 
 %% Calculate Satellite Positions over time
+fprintf('Calculating satellite positions over time...\n')
 satellitePosition = satellite.getPosition(time);
 
 %% Calculate WAAS reference station observations
-numReferenceStations = length(wrs.Users);
+fprintf('Calculating WAAS reference station observations...\n')
+numReferenceStations = length(wrsGrid.Users);
 wrsObservation(numReferenceStations, timeLength) = maast.SBASReferenceObservation;
 for i = 1:numReferenceStations
-    i
-    wrsObservation(i,:) = maast.SBASReferenceObservation(wrs.Users(i), satellitePosition);
+    wrsObservation(i,:) = maast.SBASReferenceObservation(wrsGrid.Users(i), satellitePosition);
 end
-
 %% Calculate SBAS User Observations
+fprintf('Calculating WAAS user observations...\n')
 numSBASUsers = length(sbasUserGrid.Users);
 sbasUserObservation(numSBASUsers, timeLength) = maast.SBASUserObservation;
 for i = 1:numSBASUsers
-    i
     sbasUserObservation(i,:) = maast.SBASUserObservation(sbasUserGrid.Users(i), satellitePosition);
 end
 
