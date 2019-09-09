5300a636c1f06b5105e3280087ccdcc74e5ed8a7
Andrew Neish <amneish@stanford.edu>
2019-09-09 08:11:10 -0700

    First initial working version of getIPP.m

--- maast/+maast/@SBASUserObservation/getIPP.m
+++ maast/+maast/@SBASUserObservation/getIPP.m
@@ -3,12 +3,18 @@ function getIPP(obj)
 % points. This code is from the original maast and will likely be changed
 % or replaced in the future.
 
-ll_usr = obj.UserLL;
+% Handle empty input
+if (isempty(obj.UserLL))
+    return;
+end
+
+ll_usr = obj.UserLL';
 maastConstants = maast.constants.MAASTConstants;
 ionoRadius = maastConstants.IonoRadius;
 earthConstants = sgt.constants.EarthConstants;
 earthRadius = earthConstants.R;
 el = obj.ElevationAngles(obj.SatellitesInViewMask);
+az = obj.AzimuthAngles(obj.SatellitesInViewMask);
 
 
 [n_los tmp]=size(el);
@@ -27,16 +33,13 @@ latlon(:,2)=reshape(t1,n_los,1);
 [t1 t2]=meshgrid(ll_usr(:,1),sat_idx);
 latlon(:,1)=reshape(t1,n_los,1);
 
-%initialize return value
-ll_ipp=ll_usr;
-
 %calcualte earth angle
 psi_pp=0.5*pi-el-asin(earthRadius*cos(el)/ionoRadius);
 sin_psi_pp=sin(psi_pp);
 
 %calulate IPP latitude
 ll_ipp(:,1)=asin(sin(ll_usr(:,1)).*cos(psi_pp) +...
-                 cos(ll_usr(:,1)).*sin_psi_pp.*cos(az));
+    cos(ll_usr(:,1)).*sin_psi_pp.*cos(az));
 
 %calulate IPP longitude
 ll_ipp(:,2)=ll_usr(:,2) + asin(sin_psi_pp.*sin(az)./cos(ll_ipp(:,1)));
