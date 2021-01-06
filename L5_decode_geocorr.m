e9395df2366ac7385e52026a8f70ab58551c02f8
toddwalter <twalter@stanford.edu>
2021-01-06 14:07:14 -0800

    added almanac positioning
    can now obtain geo positions from almanac if ephemeris data not available

--- L5_decode_geocorr.m
+++ L5_decode_geocorr.m
@@ -37,61 +37,71 @@ global L5MOPS_MT37_PATIMEOUT
 %loop over all of the geo data channels
 ngeos = length(svdata);
 
-for mdx = 1:ngeos
+for gdx = 1:ngeos
 
     %Initialize satellite correction data
-    svdata(mdx).geo_xyzb = NaN(size(svdata(mdx).geo_xyzb)); 
+    svdata(gdx).geo_xyzb = NaN(size(svdata(gdx).geo_xyzb)); 
 
     %Must have valid MT 37 message in order to have valid position
-    if svdata(mdx).mt37(1).time >= (time - L5MOPS_MT37_PATIMEOUT)
+    if svdata(gdx).mt37(1).time >= (time - L5MOPS_MT37_PATIMEOUT)
 
 
         %loop over all possible IODG values
         for idx = 1:4
             %Initialize satellite correction data
-            svdata(mdx).mt3940(idx).xyzb = NaN(size(svdata(mdx).mt3940(idx).xyzb));
+            svdata(gdx).mt3940(gdx, idx).xyzb = NaN(size(svdata(gdx).mt3940(idx).xyzb));
             
             %Must have valid and matching MT 39 & 40 messages
-            if svdata(mdx).mt39(idx).time >= (time - svdata(mdx).mt37(1).Ivalid3940) && ...
-                svdata(mdx).mt40(idx).time >= (time - svdata(mdx).mt37(1).Ivalid3940) && ...
-                 svdata(mdx).mt39(idx).iodg == svdata(mdx).mt40(idx).iodg
+            if svdata(gdx).mt39(idx).time >= (time - svdata(gdx).mt37(1).Ivalid3940) && ...
+                svdata(gdx).mt40(idx).time >= (time - svdata(gdx).mt37(1).Ivalid3940) && ...
+                 svdata(gdx).mt39(idx).iodg == svdata(gdx).mt40(idx).iodg
 
                 %set the prn and SPID values
-                if isnan(svdata(mdx).geo_prn_time) || ...
-                         svdata(mdx).mt39(idx).time > svdata(mdx).geo_prn_time
-                    svdata(mdx).geo_prn = svdata(mdx).mt39(idx).prn;
-                    svdata(mdx).geo_spid = svdata(mdx).mt39(idx).spid;
-                    svdata(mdx).geo_prn_time = svdata(mdx).mt39(idx).time;
+                if isnan(svdata(gdx).geo_prn_time) || ...
+                         svdata(gdx).mt39(idx).time > svdata(gdx).geo_prn_time
+                    svdata(gdx).geo_prn = svdata(gdx).mt39(idx).prn;
+                    svdata(gdx).geo_spid = svdata(gdx).mt39(idx).spid;
+                    svdata(gdx).geo_prn_time = svdata(gdx).mt39(idx).time;
                 end
 
                 %find the geo position
-                tmt0 = time - svdata(mdx).mt40(idx).te;
-                tmt0 = tmt0 - svdata(mdx).mt39(idx).agf0 - svdata(mdx).mt39(idx).agf1*tmt0;
-                svdata(mdx).mt3940(idx).xyzb(1:3) = sbas_geoeph2satpos(time, ...
-                                    svdata(mdx).mt39(idx), svdata(mdx).mt40(idx));
+                tmt0 = time - svdata(gdx).mt40(idx).te;
+                tmt0 = tmt0 - svdata(gdx).mt39(idx).agf0 - svdata(gdx).mt39(idx).agf1*tmt0;
+                svdata(gdx).mt3940(gdx, idx).xyzb(1:3) = sbas_geoeph2satpos(time, ...
+                                    svdata(gdx).mt39(idx), svdata(gdx).mt40(idx));
 
                 %put in the GEO clock  !!!!!NOT REALLY SURE THIS IS CORRECT!!!!
-                svdata(mdx).mt3940(idx).xyzb(4) = svdata(mdx).mt39(idx).agf0 + ...
-                                         svdata(mdx).mt39(idx).agf1*tmt0;
+                svdata(gdx).mt3940(gdx, idx).xyzb(4) = svdata(gdx).mt39(idx).agf0 + ...
+                                         svdata(gdx).mt39(idx).agf1*tmt0;
+                svdata(gdx).mt3940(gdx, idx).time = max([svdata(gdx).mt39(idx).time ...
+                                              svdata(gdx).mt40(idx).time]);
             end
         end
     end
     
     %process the almanac messages
-    for i = length(svdata(mdx).mt47alm):-1:1
-        mt47time(i) = svdata(mdx).mt47alm(i).time;
+    for i = length(svdata(gdx).mt47alm):-1:1
+        mt47time(i) = svdata(gdx).mt47alm(i).time;
     end
     idx = find(mt47time >= (time - 3600*24*7));
     %prn data might also be found in almanac data  %%%%TODO check that it
     %%%%%%isn't there more than once or contradicts ephemeris prn/spid
     if ~isempty(idx)
         for i = 1:length(idx)
-            if svdata(mdx).mt47alm(idx(i)).brid
-                if isnan(svdata(mdx).geo_prn_time) || ...
-                     svdata(mdx).mt47alm(idx(i)).time > svdata(mdx).geo_prn_time
-                    svdata(mdx).geo_prn = svdata(mdx).mt47alm(idx(i)).prn;
-                    svdata(mdx).geo_spid = svdata(mdx).mt47alm(idx(i)).spid;
-                    svdata(mdx).geo_prn_time = svdata(mdx).mt47alm(idx(i)).time;
+            %set the PRN value if no MT 39 or not already set
+            if svdata(gdx).mt47alm(idx(i)).brid
+                if isnan(svdata(gdx).geo_prn_time) || ...
+                     svdata(gdx).mt47alm(idx(i)).time > svdata(gdx).geo_prn_time
+                    svdata(gdx).geo_prn = svdata(gdx).mt47alm(idx(i)).prn;
+                    svdata(gdx).geo_spid = svdata(gdx).mt47alm(idx(i)).spid;
+                    svdata(gdx).geo_prn_time = svdata(gdx).mt47alm(idx(i)).time;
+                end
+            end
+            %set the GEO position (may be overwritten later by MT39/40 content
+            for cdx = 1:ngeos
+                if svdata(gdx).mt47alm(idx(i)).prn == svdata(cdx).geo_prn
+                    svdata(gdx).geo_xyzb(cdx,1:3) = sbas_geoalm2satpos(time, ...
+                                    svdata(gdx).mt47alm(idx(i)));
                 end
             end
         end
@@ -100,9 +110,12 @@ end
 
 %load position and degradation factor into the other streams data
 %assumes that the masks are all the same and in the same order as the geo data stream
-for mdx = 1:ngeos 
-    idx = setdiff(1:ngeos, mdx);
+for gdx = 1:ngeos 
+    idx = setdiff(1:ngeos, gdx);
     for i = idx
-        svdata(mdx).geo_xyzb(i,:) = svdata(i).geo_xyzb(i,:);
+        for iodg = 1:4
+            svdata(gdx).mt3940(i, iodg).xyzb = svdata(i).mt3940(i, iodg).xyzb;
+            svdata(gdx).mt3940(i, iodg).time = svdata(i).mt3940(i, iodg).time;
+        end
     end
 end
