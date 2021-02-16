ef61515f804a876062c8eae14507c4d5a2b7b186
Spencer Paul <spaul2@stanford.edu>
2021-02-16 12:22:42 -0700

    Implemented Authenticator ECDSA class
    This includes Authenticator superclass, cryptographic primatives (HMAC and SHA-256), and unit tests for both.

--- auth/Authenticator.m
+++ auth/Authenticator.m
@@ -1,18 +1,49 @@
 classdef Authenticator < handle
     %AUTHENTICATOR Contains all methods and properties pertina
     
-    properties(Access=private)
+    properties(Access=protected)
         public_key;
         private_key;
     end
     
+    methods (Access=public)
+        function obj = Authenticator(varargin)
+            switch nargin
+                case 0
+                    [private_key, public_key] = obj.derive_random_key();
+                    obj.public_key = public_key;
+                    obj.private_key = private_key;
+                case 2
+                    key = varargin{1};
+                    is_private_key = varargin{2};
+                    if is_private_key
+                        obj.private_key = key;
+                        obj.public_key = obj.derive_public_key();
+                    else
+                        obj.public_key = key;
+                        
+                    end
+                    
+                otherwise
+                    error('Authenticator:BadConstructorArguments', ...
+                    'Authenticator Constructor must have either 0 or 2 arguments')
+                    
+            end
+            
+        end
+        
+    end
     methods(Abstract, Access=public)
-        signature = sign(obj, message)
-        verified = verify(obj, message, signature)
+        signature = sign(obj, message);
+        verified = verify(obj, message, signature);
     end
     
     methods(Static, Abstract, Access=public)
-        s = salt(signature)
+        s = salt(signature);
+    end
+    
+    methods(Static, Abstract, Access=protected)
+        [private_key, public_key] = derive_random_key();
     end
 end
 
---
+++ auth/test/TestAuthenticatorECDSA.m
@@ -0,0 +1,67 @@
+classdef TestAuthenticatorECDSA < matlab.unittest.TestCase
+    %TESTAUTHENTICATORECDSA Tests for AuthenticatorECDSA
+    
+    methods(Test)       
+        function test_authenticator_ECDSA(testCase)
+            message = matlab.net.base64decode(matlab.net.base64encode("Hello World"));
+            obj = AuthenticatorECDSA();
+            sig = sign(obj, message);
+            
+            testCase.assertTrue(verify(obj, message,sig));
+        end
+        
+        function test_authenticator_ECDSA2(testCase)
+            message = matlab.net.base64decode(matlab.net.base64encode("Hello World"));
+            obj = AuthenticatorECDSA();
+            sig = sign(obj, message);
+                     
+            testCase.assertTrue(~verify(obj, message - 1,sig));
+        end
+        
+        function test_modified_signiture(testCase)
+            message = matlab.net.base64decode(matlab.net.base64encode("Hello World"));
+            obj = AuthenticatorECDSA();
+            sig = sign(obj, message);
+            sig = sig - 1;
+            
+            testCase.verifyError(@()verify(obj, message - 1, sig), ...
+            'MATLAB:Java:GenericException');
+        end
+        
+        function test_sign_multiple_messages(testCase)
+            m1 = matlab.net.base64decode(matlab.net.base64encode("Hello World"));
+            m2 = matlab.net.base64decode(matlab.net.base64encode("Hey"));
+            m3 = matlab.net.base64decode(matlab.net.base64encode("matlab"));
+            m4 = matlab.net.base64decode(matlab.net.base64encode("maast"));
+            m5 = matlab.net.base64decode(matlab.net.base64encode("tesla"));
+            
+            ae = AuthenticatorECDSA();
+            
+            sig1 = sign(ae, m1);
+            sig2 = sign(ae, m2);
+            sig3 = sign(ae, m3);
+            sig4 = sign(ae, m4);
+            sig5 = sign(ae, m5);
+            
+            testCase.assertTrue(verify(ae, m1,sig1));
+            testCase.assertTrue(verify(ae, m2,sig2));
+            testCase.assertTrue(verify(ae, m3,sig3));
+            testCase.assertTrue(verify(ae, m4,sig4));
+            testCase.assertTrue(verify(ae, m5,sig5));
+            
+            testCase.assertTrue(~verify(ae, m1, sig2));
+            testCase.assertTrue(~verify(ae, m2, sig4));
+        end
+        
+        function wrong_authenticator_test(testCase)
+            m1 = matlab.net.base64decode(matlab.net.base64encode("Hello World"));
+                      
+            ae = AuthenticatorECDSA();
+            ae2 = AuthenticatorECDSA();
+            
+            sig1 = sign(ae, m1);
+            testCase.assertTrue(verify(ae, m1, sig1));
+            testCase.assertTrue(~verify(ae2, m1, sig1));  
+        end
+    end
+end
