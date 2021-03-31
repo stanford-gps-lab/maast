19f77ac6d9b2ff4ba99f137834d3d239fd24b267
Jason Anderson <jand271@stanford.edu>
2021-03-31 15:27:38 -0700

    Added comments to checkcode build checker and removed unused report lines

--- .github/workflows/assert_matlab_checkcode.m
+++ .github/workflows/assert_matlab_checkcode.m
@@ -1,4 +1,5 @@
 function assert_matlab_checkcode(filepath)
+% executes matlab checkcode on all m files in filepath, prints any errors to screen, and then asserts no errors.
 
 matlab_files_to_check = dir(fullfile(filepath,'/**/*.m'));
 
@@ -6,10 +7,13 @@ fprintf('\n========== Executing Matlab checkcode ==========\n');
 
 failure_flag = false;
 
+% iterate among files
 for i = 1:length(matlab_files_to_check)
+    
     fullpath = fullfile(matlab_files_to_check(i).folder,matlab_files_to_check(i).name);
     error_messages = checkcode(fullpath, '-id','-string');
     
+    % report on errors
     fprintf('--> %s\n', fullpath);
     if ~isempty(error_messages)
         fprintf(error_messages);
@@ -21,6 +25,7 @@ end
 
 fprintf('\n========== Completed Matlab checkcode ==========\n');
 
+% if any failures, exit with nonzero code to report to build checker
 if failure_flag
     exit(1);
 end
